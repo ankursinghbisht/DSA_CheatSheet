0211c52f469c55438d1327e5f787524d15928a1c
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-08-03 22:32:45 +0530

    finding duplicate element in n+1 element

--- Python/11_single_duplicate_element.py
+++ Python/11_single_duplicate_element.py
@@ -5,11 +5,18 @@ Starting from index 0, going to element at value of that index and marking that 
 We'll get one positive element ( as one element is repeating)
 """
 
+
 def find_Duplicate_Element(arr):
-    return arr[0]
+    for i in range(len(arr)):
+        curr = abs(arr[i]) - 1
+
+        if arr[curr] < 0:
+            return curr + 1
+        arr[curr] *= -1
+    return -1
 
 
 if __name__ == '__main__':
-    array=[ 3,1,3,4,2]
-    ans=find_Duplicate_Element(array)
+    array = [3, 1, 3, 4, 2]
+    ans = find_Duplicate_Element(array)
     print(ans)
