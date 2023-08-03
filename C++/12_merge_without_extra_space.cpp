37aebf14ee10f421936837645a25a447eea2b578
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-08-04 01:57:18 +0530

    merging 2 sorted arrays without extra space,without sorting

--- C++/12_merge_without_extra_space.cpp
+++ C++/12_merge_without_extra_space.cpp
@@ -3,16 +3,19 @@ Idea:
 Separating elements which will be in first or second array in final answer,
 then sorting individual elements.
 
-Steps:
+Steps (while sorting after separating):
 -setting up indices as last element of 1st array and first element of 2nd array
 -traversing & swapping elements, if 1st array has larger elements
 -sorting arrays
+
+Steps (merging without sorting):
 */
 
 #include<bits/stdc++.h>
 using namespace std;
 
-void merge(int arr1[], int arr2[], int n, int m)
+
+void merge_with_sorting(int arr1[], int arr2[], int n, int m)
 {
 
     //setting up indices
@@ -34,6 +37,51 @@ void merge(int arr1[], int arr2[], int n, int m)
     sort(arr2, arr2 + m);
 
 }
+
+//helper function
+void swap_if_greater(int arr1[], int arr2[], int index1, int index2)
+{
+    // function to check if element on first array is greater & swapping if any
+    if (arr1[index1] > arr2[index2])
+        swap(arr1[index1], arr2[index2]);
+
+}
+
+void merge_without_sorting(int arr1[], int arr2[], int n, int m)
+{
+    int len = n + m;
+    int gap = len / 2 + len % 2;
+
+    while (gap > 0)
+    {
+        int left = 0, right = left + gap;
+
+        while (right < len)
+        {
+
+            if (left<n && right>n)
+                //case 1: left element from arr1 & right from arr2
+                swap_if_greater(arr1, arr2, left, right - n);
+            else if (left > n && right > n)
+                //case 2: both elements are from arr2
+                swap_if_greater(arr2, arr2, left - n, right - n);
+            else
+                //case 3:both elements are from arr1
+                swap_if_greater(arr1, arr1, left, right);
+            ++left;
+            ++right;
+        }
+
+        if (gap == 1)
+            break;
+
+        gap = gap / 2 + gap % 2; //updating gap value 
+    }
+}
+
+
+
+
 int main()
 {
     int t;
@@ -52,7 +100,7 @@ int main()
         for (int i = 0;i < m;i++)
             scanf("%d", &arr2[i]);
 
-        merge(arr1, arr2, n, m);
+        merge_without_sorting(arr1, arr2, n, m);
         for (int i = 0;i < n;i++)
             printf("%d ", arr1[i]);
         for (int i = 0;i < m;i++)
