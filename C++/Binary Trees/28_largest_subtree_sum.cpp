7098efad1565313e69b49074f2fa3e00fbce92af
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-11-03 00:49:54 +0530

    largest subtree sum (Python)

---
+++ C++/Binary Trees/28_largest_subtree_sum.cpp
@@ -0,0 +1,134 @@
+/*
+Idea:
+doing post-order traversal, with keeping track of sum of left and right subtree of a node, and keeping track of max value.
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+public:
+    // Function to find largest subtree sum.
+    int ans = INT_MIN;
+
+    int solve(Node* root)
+    {
+        // If current node is null then  return 0 to parent node.
+        if (root == NULL)
+            return 0;
+
+        // Subtree sum rooted at current node.
+        int leftSum = solve(root->left);
+        int rightSum = solve(root->right);
+        int sum = leftSum + rightSum + root->data;
+
+        // Update answer if current subtree  sum is greater than answer so far.
+        ans = max(ans, sum);
+
+        // Return current subtree sum to its parent node.
+        return sum;
+
+    }
+    int findLargestSubtreeSum(Node* root)
+    {
+        solve(root);
+        return ans;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+int main() {
+    int t;
+    scanf("%d\n", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.findLargestSubtreeSum(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
