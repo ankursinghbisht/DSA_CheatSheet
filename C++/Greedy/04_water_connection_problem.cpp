c7deafd9fe1c97887decc60e24c30ce7ac827c4b
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-11-17 11:35:41 +0530

    Water Connection problem (C++)

--- C++/Greedy/04_water_connection_problem.cpp
+++ C++/Greedy/04_water_connection_problem.cpp
@@ -1,5 +1,13 @@
 /*
+Idea:
+-Perform DFS from appropriate houses to find all different connected components.
+The number of different connected components is our answer t.
 
+-The next t lines of the output are the beginning of the connected component,
+end of the connected component and the minimum diameter from the start to the end of the connected component in each line.
+
+-Since, tanks can be installed only on the houses having outgoing pipe and no incoming pipe,
+therefore these are appropriate houses to start DFS from i.e. perform DFS from such unvisited houses.
 */
 
 //{ Driver Code Starts
@@ -10,9 +18,63 @@ using namespace std;
 class Solution
 {
 public:
+
+    // number of houses and numberof pipes
+    int n, p;
+    // Array rd stores the ending vertex of pipe
+    vector<int> rd;
+    // Array wd stores the value of diameters between two pipes
+    vector<int> wt;
+    // Array cd stores the starting end of pipe
+    vector<int> cd;
+    int ans;
+
+    int dfs(int w)
+    {
+        if (cd[w] == 0)
+            return w;
+        if (wt[w] < ans)
+            ans = wt[w];
+        return dfs(cd[w]);
+    }
+
     vector<vector<int>> solve(int n, int p, vector<int> a, vector<int> b, vector<int> d)
     {
-        // code here
+        this->n = n;
+        this->p = p;
+        rd = vector<int>(n + 1);
+        wt = vector<int>(n + 1);
+        cd = vector<int>(n + 1);
+        vector<vector<int>> res;
+
+        int i = 0;
+
+        while (i < p)
+        {
+
+            int q = a[i], h = b[i],
+                t = d[i];
+
+            cd[q] = h;
+            wt[q] = t;
+            rd[h] = q;
+            i++;
+        }
+
+
+        for (int j = 1; j <= n; ++j)
+
+            //If a pipe has no ending vertex but has starting vertex i.e is an outgoing pipe then we need to start DFS with this vertex.
+            if (rd[j] == 0 && cd[j]) {
+                ans = 1000000000;
+                int w = dfs(j);
+
+                // We put the details of component in final output res array
+
+                res.push_back({ j,w,ans });
+            }
+
+        return res;
     }
 };
 
