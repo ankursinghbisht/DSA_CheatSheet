cc3507b81637cc88a7bfffad00838a16f21a57b1
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-11-25 11:52:56 +0530

    Distributing N chocolate packs amon M students (Python)

---
+++ Python/Greedy/23_chocolate_distibution_problem.py
@@ -0,0 +1,36 @@
+"""
+Idea:
+to sort in ascending order, and finding subarray of size 'M', whose element difference of 1st and last is min.
+"""
+
+
+# User function Template for python3
+
+class Solution:
+
+    def findMinDiff(self, A, N, M):
+
+        A.sort()
+        ans = float("inf")
+        for i in range(M - 1, N):
+            ans = min(ans, A[i] - A[i - M + 1])
+        return ans
+
+
+# {
+# Driver Code Starts
+# Initial Template for Python 3
+
+if __name__ == '__main__':
+
+    t = int(input())
+
+    for _ in range(t):
+        N = int(input())
+        A = [int(x) for x in input().split()]
+        M = int(input())
+
+        solObj = Solution()
+
+        print(solObj.findMinDiff(A, N, M))
+# } Driver Code Ends
