f98b563117b6a84424563691309992a6d63be64c
Ankur Singh Bisht <ankursinghbisht007@gmail.com>
2023-12-13 16:47:34 +0530

    Implementing LRU cache (C++)

---
+++ C++/Stacks & Queues/25_LRU_cache.cpp
@@ -0,0 +1,158 @@
+/*
+Idea:
+using Doubly LL to keep track of least to most used key & hashmap to access value of key.
+when cache gets full delete from tail.
+*/
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+struct Node {
+    int key;
+    int value;
+    Node* next, * pre;
+    Node(int key, int value) {
+        this->key = key;
+        this->value = value;
+        next = pre = NULL;
+    }
+};
+
+class LRUCache
+{
+private:
+    static unordered_map<int, Node*> hsmap;
+    static int capacity, count;
+    static Node* head, * tail;
+
+public:
+    //Constructor for initializing the cache capacity with the given value.
+    LRUCache(int cap)
+    {
+        unordered_map<int, Node*> temp;
+        hsmap = temp;
+        capacity = cap;
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head->next = tail;
+        head->pre = NULL;
+        tail->next = NULL;
+        tail->pre = head;
+        count = 0;
+    }
+
+    void addToHead(Node* node)
+    {
+        node->next = head->next;
+        node->next->pre = node;
+        node->pre = head;
+        head->next = node;
+    }
+
+    //Function to delete a node.
+    void deleteNode(Node* node)
+    {
+        node->pre->next = node->next;
+        node->next->pre = node->pre;
+    }
+
+    //Function to return value corresponding to the key.
+    int GET(int key)
+    {
+        //if element is present in map,
+        if (hsmap.count(key) > 0)
+        {
+            Node* node = hsmap[key];
+            int result = node->value;
+
+            deleteNode(node);
+            addToHead(node);
+
+            //returning the value.
+            return result;
+        }
+        //else we return -1.
+        return -1;
+    }
+
+    //Function for storing key-value pair.
+    void SET(int key, int value)
+    {
+        if (hsmap.count(key) > 0)
+        {
+            Node* node = hsmap[key];
+            node->value = value;
+            deleteNode(node);
+            addToHead(node);
+        }
+        else
+        {
+            Node* node = new Node(key, value);
+            hsmap[key] = node;
+            if (count < capacity) {
+                count++;
+                addToHead(node);
+            }
+            else {
+                hsmap.erase(tail->pre->key);
+                deleteNode(tail->pre);
+                addToHead(node);
+            }
+        }
+    }
+};
+
+//initializing static members.
+unordered_map<int, Node*> temp;
+int LRUCache::capacity = 0;
+Node* LRUCache::head = new Node(0, 0);
+Node* LRUCache::tail = new Node(0, 0);
+int LRUCache::count = 0;
+unordered_map<int, Node*> LRUCache::hsmap = temp;
+
+
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int capacity;
+        cin >> capacity;
+        LRUCache* cache = new LRUCache(capacity);
+
+        int queries;
+        cin >> queries;
+        while (queries--)
+        {
+            string q;
+            cin >> q;
+            if (q == "SET")
+            {
+                int key;
+                cin >> key;
+                int value;
+                cin >> value;
+                cache->SET(key, value);
+            }
+            else
+            {
+                int key;
+                cin >> key;
+                cout << cache->GET(key) << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
